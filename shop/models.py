00c3aaa624daf441347f443a79fbad43276ea742
Full Due <fulldue@example.com>
2022-12-08 23:35:16 +0100

    Created Templates

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class HeroPhoto(models.Model):
+    slogan = models.CharField(max_length=200)
+    text = models.CharField(max_length=500)
+    photo = models.ImageField(upload_to='media/hero',
+                              blank=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    available = models.BooleanField(default=True)
+
+
+class CollectionProduct(models.Model):
+    name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='media/collection',
+                              blank=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    available = models.BooleanField(default=True)
+
+
+
+class FeaturedProduct(models.Model):
+    name = models.CharField(max_length=50)
+    product_name = models.CharField(max_length=50)
+    text = models.CharField(max_length=50)
+    available = models.BooleanField(default=True)
+
+
+
+class SaleProduct(models.Model):
+    name = models.CharField(max_length=50)
+    sale_date = models.DateField()
+    sale_detail = models.CharField(max_length=500)
+    slug = models.SlugField(max_length=200, db_index=True)
+    sale_image = models.ImageField(upload_to='media/sale',
+                              blank=True)
+
+
+
+class PromoProduct(models.Model):
+    name = models.CharField(max_length=50)
+    promo_start_date = models.DateField()
+    promo_end_date = models.DateField()
+    promo_image = models.ImageField(upload_to='media/promo',
+                              blank=True)
--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import LandingPageView
+
+app_name = 'shop'
 
 urlpatterns = [
-
+    path('', LandingPageView.as_view(), name='landing-page')
 ]
